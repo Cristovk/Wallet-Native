f3dc5dd61da5620cac965da7c05bd7f234371b32
HernanSsj <hernanns46@gmail.com>
2020-11-10 16:13:27 -0300

    iconos

--- client/Screen/TransactionHistory/Movimientos.js
+++ client/Screen/TransactionHistory/Movimientos.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState} from 'react'
 import { View,ScrollView } from 'react-native'
-import {ButtonGroup} from 'react-native-elements'
+import {ButtonGroup, Overlay, Button, Text, Icon} from 'react-native-elements'
 import style from "./Movimientos_Styles"
 import historial from "./utils"
 //import { PieChart} from 'react-native-svg-charts'
@@ -9,11 +9,9 @@ const Movimientos = ({ navigation }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [list, setList] = useState([]);
   const buttons = ['Hoy', 'Semana', 'Mes']
-
   const lista2 = [
     {
       title: 'Netflix',
-      icon: 'av-timer',
       amount: 645,
       purchaseId: 1,
       type: "Entretenimiento"
@@ -22,43 +20,73 @@ const Movimientos = ({ navigation }) => {
   const lista1 = [
     {
       title: 'Netflix',
-      icon: 'av-timer',
       amount: 645,
       purchaseId: 1,
       type: "Entretenimiento"
     },
     {
       title: 'Medialunas del abuelo',
-      icon: 'flight-takeoff',
       amount: 5000,
       purchaseId: 2,
-      type: "Almacen"
+      type: "Panaderia"
+    },
+    {
+      title: 'Supermarine Spitfire',
+      amount: 100000000,
+      purchaseId: 3,
+      type: "Jet"
+    },
+    {
+      title: 'Curitas',
+      amount: 150,
+      purchaseId: 4,
+      type: "Farmacia"
     }]
   const lista = [
     {
       title: 'Netflix',
-      icon: 'av-timer',
       amount: 645,
       purchaseId: 1,
       type: "Entretenimiento"
     },
     {
       title: 'Medialunas del abuelo',
-      icon: 'flight-takeoff',
+      amount: 5000,
+      purchaseId: 2,
+      type: "Panaderia"
+    },
+    {
+      title: 'Supermarine Spitfire',
+      amount: 100000000,
+      purchaseId: 3,
+      type: "Jet"
+    },
+    {
+      title: 'Curitas',
+      amount: 150,
+      purchaseId: 4,
+      type: "Farmacia"
+    },
+    {
+      title: 'Netflix',
+      amount: 645,
+      purchaseId: 1,
+      type: "Entretenimiento"
+    },
+    {
+      title: 'Medialunas del abuelo',
       amount: 5000,
       purchaseId: 2,
       type: "Panaderia"
     },
     {
       title: 'Carga sube',
-      icon: 'flight-takeoff',
       amount: 98,
       purchaseId: 3,
-      type: "Transporte publico"
+      type: "Transporte"
     },
     {
       title: 'Super Dia%',
-      icon: 'flight-takeoff',
       amount: 789,
       date: "Sun Nov 08 2020 16:49:19 GMT-0300 (hora estándar de Argentina)",
       purchaseId: 4,
@@ -66,7 +94,6 @@ const Movimientos = ({ navigation }) => {
     },
     {
       title: 'Factura Personal',
-      icon: 'flight-takeoff',
       amount: 340,
       purchaseId: 5,
       type: "Servicios"
@@ -74,66 +101,54 @@ const Movimientos = ({ navigation }) => {
     },
     {
       title: 'Riot Points',
-      icon: 'flight-takeoff',
       amount: 560,
       purchaseId: 6,
       type: "Videojuegos"
     },
     {
       title: 'Riot Points',
-      icon: 'flight-takeoff',
       amount: 560,
       purchaseId: 6,
       type: "Videojuegos"
     },
     {
-      title: 'Riot Points',
-      icon: 'flight-takeoff',
-      amount: 560,
+      title: 'Shell',
+      amount: 7800,
       purchaseId: 6,
-      type: "Videojuegos"
+      type: "Gasolinera"
     },
     {
       title: 'Riot Points',
-      icon: 'flight-takeoff',
       amount: 560,
       purchaseId: 6,
       type: "Videojuegos"
     },
     {
       title: 'Netflix',
-      icon: 'av-timer',
       amount: 645,
       purchaseId: 1,
       type: "Entretenimiento"
     },
     {
       title: 'Medialunas del abuelo',
-      icon: 'flight-takeoff',
       amount: 5000,
       purchaseId: 2,
       type: "Panaderia"
     },
     {
       title: 'Netflix',
-      icon: 'av-timer',
       amount: 645,
       purchaseId: 1,
       type: "Entretenimiento"
     },
     {
       title: 'Medialunas del abuelo',
-      icon: 'flight-takeoff',
       amount: 5000,
       purchaseId: 2,
       type: "Panaderia"
     }
     
   ]
-
-  
-
-
   useEffect(()=>{
     setList(lista)
     if(selectedIndex==0){ setList(lista2)}
@@ -144,17 +159,16 @@ const Movimientos = ({ navigation }) => {
   return (
    
       <View>
-
-        <View >
-          <ButtonGroup
-              onPress={setSelectedIndex}
-              selectedIndex={selectedIndex}
-              buttons={buttons}
-              containerStyle={{height: 50}}
-          />
-        </View>
-      
-        <ScrollView style={style.lista}>{historial(list, { navigation })}</ScrollView>  
+          <View >
+              <ButtonGroup
+                  onPress={setSelectedIndex}
+                  selectedIndex={selectedIndex}
+                  buttons={buttons}
+                  containerStyle={{height: 50}}
+              />
+          </View>
+        
+          <ScrollView style={style.lista}>{historial(list, { navigation })}</ScrollView>  
 
     </View>
   )
--- client/Screen/TransactionHistory/utils.js
+++ client/Screen/TransactionHistory/utils.js
@@ -1,13 +1,15 @@
 import React from 'react'
-import { ListItem, Icon } from 'react-native-elements'
+import { ListItem} from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 import { View, Text, ScrollView, Button, Image, StyleSheet, TouchableOpacity, Alert } from 'react-native'
 const historial = (lista,{ navigation }) => {
-
+    const iconList ={Panaderia:"cookie", Almacen:"shopping-basket",Videojuegos:"gamepad", Entretenimiento:"play-circle", 
+                     Transporte:"bus-alt", Gasolinera:"gas-pump", Jet:"fighter-jet", Farmacia:"first-aid", Servicios:"file-invoice-dollar"}
    return lista.map((item, i) => (
       <ListItem onPress={() => navigation.navigate("Detalle", {
         purchaseId: item.purchaseId
       })} key={i} bottomDivider>
-        <Icon name={item.icon} />
+        <Icon name={iconList[item.type]} size={30} color="black" />
         <ListItem.Content >
           <ListItem.Title >{item.title}</ListItem.Title>
           <ListItem.Subtitle>{`${item.type}`}</ListItem.Subtitle>
