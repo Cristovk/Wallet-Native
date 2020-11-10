510889ac5bed8f90210842ca54a89a10c2656d09
Cristovk <63594174+Cristovk@users.noreply.github.com>
2020-11-10 20:30:14 -0300

    Rutas funcionales de ejemplo

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/functions'
-import * as admin from 'firebase-admin';
+
 
 
 const db = firebase.initializeApp({
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,93 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const ex = express();
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+
+admin.initializeApp({
+    credential: admin.credential.cert('./permissions.json'),
+    databaseURL: "https://henrybankfire.firebaseio.com"
+  });
+
+  const  DBS  = admin.firestore()
+
+
+
+
+ex.get('/hello-world', (req, res) => {
+    return res.status(200).json({message: 'Hola caracola'})
+});
+
+ex.post('/api/user-data', async (req, res) =>{
+  try{
+    await  DBS.collection('user_data').doc('/' + req.body.id + '/')
+    .create({name: req.body.name});
+    return res.status(204).json();
+
+    }catch(error) {
+        console.log(error)
+        return res.status(500).send(error);
+    }
+});
+
+ex.post('/api/user-try', async (req, res) =>{
+    try{
+      await  DBS.doc('user-try/' + req.body.id + '/')
+      .create({
+          name: req.body.name,
+          age: req.body.age  
+        });
+      return res.status(204).json();
+   
+      }catch(error) {
+          console.log(error)
+          return res.status(500).send(error);
+      }
+  });
+
+  //traer un user
+
+  ex.get('/api/user-try/:user_id', async (req, res) => {
+      try{
+
+        const doc = DBS.doc('user-try/'+req.params.id);
+        const name = await doc.get()
+            const response = name.data()
+            return res.status(200).json(response)
+
+
+      }catch{
+        console.log(error)
+        return res.status(500).send(error);
+
+      }
+
+
+      
+  })
+
+  //traer todo los usuarios
+  ex.get('/api/user-try', async (req, res) => {
+    try{
+
+      const query = DBS.collection('user-try');
+      const querySnapshot = await query.get();
+      const docs = querySnapshot.docs; 
+      
+      
+      const response = docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+      }));
+
+      return res.status(200).json(response)
+
+    }catch{
+      console.log(error)
+      return res.status(500).send(error);
+
+    }
+});
+
+exports.ex = functions.https.onRequest(ex);
