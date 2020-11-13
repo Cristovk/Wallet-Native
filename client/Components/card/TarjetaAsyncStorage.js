60ae8430f122235a88aad5c23975361aefc762c5
larts85 <larts85@yahoo.com>
2020-11-12 23:36:37 -0300

    to bring master

--- client/Components/card/TarjetaAsyncStorage.js
+++ client/Components/card/TarjetaAsyncStorage.js
@@ -1,6 +1,17 @@
 import AsyncStorage from "@react-native-community/async-storage";
+import db, { functions, storage } from "../../../firebase";
 
 const TARJETA_KEY = "@tarjeta:key";
+// async function saveTarjetas(tarjetas) {
+//   console.log("saveTarjetas", tarjetas);
+//   try {
+//     const newCard = await storage.collection('Users').doc().collection('creditCards').doc();
+//     return JSON.stringify(tarjetas);
+//   } catch (error) {
+//     console.log("Error de sintaxis");
+//     return "Error de sintaxis";
+//   }
+// }
 async function saveTarjetas(tarjetas) {
   console.log("saveTarjetas", tarjetas);
   try {
