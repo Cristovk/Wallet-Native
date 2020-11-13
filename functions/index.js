c1bb91229660d00d74fad2030b3875a340b90a5e
Cristovk <63594174+Cristovk@users.noreply.github.com>
2020-11-13 11:39:31 -0300

    functions firebase  no tocar

--- functions/index.js
+++ functions/index.js
@@ -3,9 +3,14 @@ const express = require('express');
 const ex = express();
 const admin = require('firebase-admin');
 //import {auth} from '../../firebase'
+<<<<<<< Updated upstream
 
 //Cloud Functions
+=======
+>>>>>>> Stashed changes
 
+//Cloud Functions
+  
 
 
 
@@ -64,6 +69,7 @@ ex.post('/user'), (req, res) => {
 
 
 
+<<<<<<< Updated upstream
 
 ex.get('/hello-world', (req, res) => {
   return res.status(200).json({ message: 'Hola caracola' })
@@ -99,20 +105,121 @@ ex.post('/api/user-try', async (req, res) => {
 });
 
 //traer un user
+=======
+  const  DBS  = admin.firestore()
+  const auth = admin.auth()
+
+
+// ex.post('/user', (req, res)  => {
+
+//   const { email , password,  name , lastName, birtday, phone, dni, cuil} = req.body;
+//   console.log(req.body);
+
+//   //crear usuario
+//   auth.createUserWithEmailAndPassword(email, password)
+//   .then(() => {
+//     //Si el usuario se crea,los datos del registro se agregan
+//     DBS.collection('user').doc(auth.currentUser.uid)
+//     .set({
+//       name: name,
+//       lastName: lastName,
+//       birthay: birtday,
+//       phone: phone,
+//       dni: dni,
+//       cuil: cuil,
+//       created: Date.now()
+
+//     })
+//         .catch(error => {
+//           console.log('Algo salio mal al agregar el user a firestore', error)
+//           })
+
+//     return res.status(200).json();
+    
+
+//   })
+//   .catch(error => {
+//     console.log('Algo fallo en el Registro', error)
+//     return res.status(500).send(error);
+//   })
+
+
+
+// }
+
+// //put user
+// ex.put('Users/:user_id', async( req, res) => {
+
+//   try {
+//     const documents = DBS.collection('Users').doc(req.params.user_id)
+//     await documents.
+//   }
+
+// })
+
+
+
+
+
+// ex.get('/hello-world', (req, res) => {
+//     return res.status(200).json({message: 'Hola caracola'})
+// });
+
+// //Users
+
+// ex.post('/api/user-data', async (req, res) =>{
+//   try{
+//     await  DBS.collection('user_data').doc('/' + req.body.id + '/').collection(asdas).doc(adsjjsaa)
+//     .create({name: req.body.name});
+//     return res.status(204).json();
+
+//     }catch(error) {
+//         console.log(error)
+//         return res.status(500).send(error);
+//     }
+// });
+
+ex.post('/api/user-try', async (req, res) =>{
+    try{
+      await  DBS.doc('user-try/' + req.body.id + '/')
+      .create({
+          name: req.body.name,
+          age: req.body.age  
+        });
+      return res.status(204).json();
+   
+      }catch(error) {
+          console.log(error)
+          return res.status(500).send(error);
+      }
+  });
+>>>>>>> Stashed changes
 
 ex.get('/api/user-try/:user_id', async (req, res) => {
   try {
 
+<<<<<<< Updated upstream
     const doc = DBS.doc('user-try/' + req.params.id);
     const name = await doc.get()
     const response = name.data()
     return res.status(200).json(response)
 
+=======
+  // ex.get('/api/user-try/:user_id', async (req, res) => {
+  //     try{
+
+  //       const snapshot = await admin.DBS.collection('user-try')
+  //                 .doc(req.params.user_id).get();
+        
+  //       const userID = snapshot.id;
+  //           return res.status(200).send(JSON.stringify({id:userID, ...userData}))
+>>>>>>> Stashed changes
 
   } catch {
     console.log(error)
     return res.status(500).send(error);
 
+<<<<<<< Updated upstream
   }
 
 
@@ -141,7 +248,44 @@ ex.get('/api/user-try', async (req, res) => {
 
   }
 });
+=======
+  //     }catch{
+  //       console.log(error)
+  //       return res.status(500).send(error);
+
+  //     }
+
+
+      
+  // })
+
+//   //traer todo los usuarios
+//   ex.get('/api/user-try', async (req, res) => {
+//     try{
+
+//       const query = DBS.collection('user-try');
+//       const querySnapshot = await query.get();
+//       const docs = querySnapshot.docs; 
+      
+      
+//       const response = docs.map((doc) => ({
+//           id: doc.id,
+//           name: doc.data().name,
+//       }));
+
+//       return res.status(200).json(response)
+
+//     }catch{
+//       console.log(error)
+//       return res.status(500).send(error);
+
+//     }
+// });
+
+>>>>>>> Stashed changes
 
 
 
 exports.ex = functions.https.onRequest(ex);
+
+
