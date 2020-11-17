97c19d01e3b316f5ca48aaaedd2b5fd6062c066b
HernanSsj <hernanns46@gmail.com>
2020-11-17 18:33:53 -0300

    notificaciones infinitas

--- client/Screen/Home/home.js
+++ client/Screen/Home/home.js
@@ -1,11 +1,109 @@
-import React, { useEffect, useState } from 'react'
-import { View, Text, ScrollView } from 'react-native'
+import React, { useEffect, useState, useRef} from 'react'
+import { View, Text, ScrollView,Button, Platform } from 'react-native'
 import { ListItem } from 'react-native-elements'
-import { storage } from '../../../firebase'
 import style from './homeStyles'
+import Constants from 'expo-constants';
+import * as Notifications from 'expo-notifications';
+import * as Permissions from 'expo-permissions';
+import { auth, storage } from "../../../firebase";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+async function schedulePushNotification(title, body) {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title,
+     body,
+      data: { data: 'goes here' },
+    },
+    trigger: { seconds: 10 },
+  });
+}
+async function buildNotification(amount,where,type,received){
+  let title
+  let body
+
+  if(type == "Tentrante"){
+      let title = `Nueva transferencia de ${from}.`
+      let body = `Recibiste $${amount}.`
+              
+  }else if(type == "compra"){
+    let title = `Tu pago a ${where} fue aprobado.`
+    let body = `Total $${amount}.`
+                
+    }else if(type == "recarga"){
+      let title = `Nueva recarga acreditada.`
+      let body = `Total  + $${amount}.` 
+    } 
+    schedulePushNotification(title,body)
+    //post to reset state
+}
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  return token;
+}
 const Home = ({ navigation }) => {
+  const [expoPushToken, setExpoPushToken] = useState('');
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
 
+  useEffect(() => {
+    
+    storage.collection("Users")
+    .doc(auth.currentUser.uid)
+    .collection("Wallet")
+    .doc("transferReceived")
+    .onSnapshot(async function(doc) {
+      const {amount, where, type, received} = await doc.data()
+
+    });
+    registerForPushNotificationsAsync().then(token => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
   // const {title, amount, icon} = route.params;
 
   const lista = [
