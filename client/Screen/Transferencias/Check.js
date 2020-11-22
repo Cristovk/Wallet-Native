9b0a1c06f222fe0532bb29b2ad4419344a15324f
HernanSsj <hernanns46@gmail.com>
2020-11-21 22:14:12 -0300

    va queriendo ponele

--- client/Screen/Transferencias/Check.js
+++ client/Screen/Transferencias/Check.js
@@ -3,7 +3,6 @@ import {
   View,
   Text,
   ScrollView,
-  Button,
   Image,
   TouchableOpacity,
 } from "react-native";
@@ -12,73 +11,77 @@ import { Icon, ListItem } from "react-native-elements";
 
 const confirmOrError = ({ navigation, route }) => {
   const datos = route.params
- console.log("---------",datos)
- console.log("ESTO",datos.receiver)
- console.log('BIEN',route.params.receiver)
+  console.log(datos)
   return (
     <ScrollView>
-        <View style={style.tituloContainer}>
-          <Text style={style.titulo}>Datos del receptor</Text>
-        </View>
-        <View style={style.listaContenedor}>
-          <ListItem style={style.lista}>
-          
-            <ListItem.Content>
-              <ListItem.Title>Nombre:</ListItem.Title>
-              <ListItem.Subtitle>{"Lizi Tagliani"}</ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-          <ListItem style={style.lista}>
-           
-            <ListItem.Content>
-              <ListItem.Title>Dni: </ListItem.Title>
-              <ListItem.Subtitle>{"41130203"} </ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-          <ListItem style={style.lista}>
-        
-            <ListItem.Content>
-              <ListItem.Title>Telefono: </ListItem.Title>
-              <ListItem.Subtitle>{"+541198562"} </ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-          <ListItem style={style.lista}>
+      { datos.receiver ?
+          <View>
+          <View style={style.tituloContainer}>
+            <Text style={style.titulo}>Datos del receptor</Text>
+          </View>
+          <View style={style.listaContenedor}>
+            <ListItem style={style.lista}>
             
-            <ListItem.Content>
-              <ListItem.Title>Cvu: </ListItem.Title>
-              <ListItem.Subtitle>{"456789798"} </ListItem.Subtitle>
-            </ListItem.Content>
-          </ListItem>
-          <View style={[style.botonContainer, { marginBottom: 15 }]}>
+              <ListItem.Content>
+                <ListItem.Title>Nombre:</ListItem.Title>
+                <ListItem.Subtitle>{datos.receiver.nombre +" " + datos.receiver.apellido}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+            <ListItem style={style.lista}>
              
-                <TouchableOpacity
-                  style={style.boton}
-                  onPress={() => {
-
-                      navigation.navigate("Finish", {
-                       
-                      })
-                  }}
-                >
-            <Text style={{ fontWeight: "bold", fontSize: 15 }}>Confirmar</Text>
-            
-          </TouchableOpacity>
-          <TouchableOpacity
-                  style={style.boton}
-                  onPress={() => {
-
-                      navigation.navigate("Home", {
-                       
-                      })
-                  }}
-                >
-            <Text style={{ fontWeight: "bold", fontSize: 15 }}>Cancelar</Text>
-            
-          </TouchableOpacity>
+              <ListItem.Content>
+                <ListItem.Title>Dni: </ListItem.Title>
+                <ListItem.Subtitle>{datos.receiver.dni} </ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+            <ListItem style={style.lista}>
+          
+              <ListItem.Content>
+                <ListItem.Title>Telefono: </ListItem.Title>
+                <ListItem.Subtitle>{datos.receiver.telefono} </ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+            <ListItem style={style.lista}>
+              
+              <ListItem.Content>
+                <ListItem.Title>Cvu: </ListItem.Title>
+                <ListItem.Subtitle>{datos.receiver.cvu} </ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+            <View style={[style.botonContainer, { marginBottom: 15 }]}>
+               
+                  <TouchableOpacity
+                    style={style.boton}
+                    onPress={() => {
+                        navigation.navigate("Finish", {
+                         
+                        })
+                    }}
+                  >
+              <Text style={{ fontWeight: "bold", fontSize: 15 }}>Confirmar</Text>
+              
+            </TouchableOpacity>
+            <TouchableOpacity
+                    style={style.boton}
+                    onPress={() => {
+  
+                        navigation.navigate("Home")
+                    }}
+                  >
+              <Text style={{ fontWeight: "bold", fontSize: 15 }}>Cancelar</Text>
+              
+            </TouchableOpacity>
+            </View>
+            </View>
           </View>
-          </View>
+        : <View>
+          <Text>HEy Alerta de no encontre nada.png</Text>
+         
+         </View>
+      }
+        
     </ScrollView>
-  );
+  )
 };
 
 export default confirmOrError;
--- client/Screen/Transferencias/transferir.js
+++ client/Screen/Transferencias/transferir.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  ScrollView,
-  TouchableOpacity,
-  LogBox,
-  Alert,
-  ActivityIndicator,
-} from "react-native";
+import { View,TouchableOpacity,LogBox,Alert,ActivityIndicator,TextInput,Text} from "react-native";
 import style from "./transferEstilos";
-import { Icon, Text, ListItem } from "react-native-elements";
-import { TextInput } from "react-native-gesture-handler";
+import { Icon, ListItem } from "react-native-elements";
 import * as SMS from "expo-sms";
 import { auth, storage } from "../../../firebase";
 const axios = require("axios");
 const Transferencias = ({ navigation }) => {
+  
+  useEffect(()=>{
+    async function setear(){
+      const senderId = await auth.currentUser.uid;
+      setDato({ ...dato, senderId: senderId });
+    };
+    setear();
+  },[])
  
   const [state, setState] = useState(false);
   const [exist, setExist] = useState(false);
@@ -22,47 +22,37 @@ const Transferencias = ({ navigation }) => {
     receivercvu: "",
     senderId: "",
   });
-  const [spinner, setSpinner] = useState(false);
 
-  const [incomplete, setIncomplete] = useState(false);
-
-  useEffect(async () => {
-    const senderId = await auth.currentUser.uid;
-    setDato({ ...dato, senderId: senderId });
-  }, []);
+  const[spinner, setSpinner] = useState(false);
 
   const comprobarCvu = async () => {
-    try {
-      let respuesta = await axios.post(
-        "https://us-central1-henrybankfire.cloudfunctions.net/cvuExist",
-        { cvu: dato.receivercvu }
-      );
-      return respuesta;
-    } catch (error) {
-      return error;
-    }
+    try{let respuesta=await axios.post("https://us-central1-henrybankfire.cloudfunctions.net/cvuExist",{ cvu: dato.receivercvu });
+    return respuesta;
+  } catch{
+      return null
+  }
     
   };
-
-  const handleSubmit = async () => {
+ 
+  const handleSubmit = async ()=>{
     let respuesta = await comprobarCvu();
-    console.log("Tengo res", respuesta.data.datos)
-   let objeto = respuesta.data.datos;
-   console.log('DESDE OBJETO',objeto)
-   
-      // setReceiver({ ...receiver, receiver: objeto });
-      setReceiver(objeto);
-      console.log(receiver,'después de setearlo');
+    setSpinner(true);
+       setTimeout(() => {
+        setSpinner(false);
+        setDato({receivercvu: "",senderId: ""});
+        respuesta ?
+        navigation.navigate("confirmOrError", {
+                  receiver:respuesta.data.datos,
+                  dato:dato
+                })
+                :
+                navigation.navigate("confirmOrError", {
+                  receiver:null,
+                  dato:dato
+                })
+      }, 2000);
+  }
 
-
-      const { receivercvu } = dato;
-      if (!receivercvu) {
-        return setIncomplete(true);
-      }
-      setIncomplete(false);
-      setSpinner(true);
-     
-  };
   return (
     <View>
       <View style={style.cvu}>
@@ -74,37 +64,24 @@ const Transferencias = ({ navigation }) => {
         placeholder="12345678"
         keyboardType="numeric"
         style={style.input}
-        onChangeText={(data) => {
-           setDato({ ...dato, receivercvu: data });
-        }}
+        onChangeText={(data) => setDato({ ...dato, receivercvu: data })}
+        value={dato.receivercvu}
       />
-      <View style={[style.botonContainer, { marginBottom: 15 }]}>
-        {!spinner && (
-          <TouchableOpacity
-            disabled={dato.receivercvu.length < 8 ? true : false}
-            style={style.boton}
-            onPress={() => {
-              handleSubmit();
-          
-              setTimeout(() => {
-                navigation.navigate("confirmOrError", {
-                  receiver,
-                  dato:dato
-                });
-              }, 2000);
-                
-            }}
-          >
-            <Text style={{ fontWeight: "bold", fontSize: 15 }}>Continuar</Text>
-          </TouchableOpacity>
-        )}
 
-        {spinner && (
+       {spinner && (
           <View style={style.spinner}>
             <ActivityIndicator size="small" color="#fff" />
           </View>
         )}
-      </View>
+
+      {!spinner &&(
+        <View style={[style.botonContainer, { marginBottom: 15 }]}>
+          <TouchableOpacity style={style.boton} onPress={handleSubmit} disabled={dato.receivercvu.length < 8 ? true : false}>
+            <Text style={{ fontWeight: "bold", fontSize: 15 }}>Continuar</Text>
+          </TouchableOpacity>
+        </View>
+      )} 
+      
     </View>
   );
 };
